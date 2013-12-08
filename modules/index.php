fba3fc8400c5cf3e4a19bdc34652b1aa7477e4eb
Xiogoperus <xiogoperus@gmail.com>
2013-12-08 23:22:52 +0400

    Add all files

---
+++ modules/index.php
@@ -0,0 +1,9 @@
+<?php
+/*******************************************************************************
+========  ==========  ==========  ==========  ==========  ==========  ==========
+Xiogop is an open source Content Management System (CMS) provided free for use
+ under the GNU General Public License.					
+                                             === xiogoperus@gmail.com ===
+========  ==========  ==========  ==========  ==========  ==========  ==========
+*******************************************************************************/
+defined('_XIO') or die('Possible hack attempt!');
